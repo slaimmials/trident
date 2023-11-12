3528cec80e0849e2f563fdefe730c7c2c9626457
slaimmials <77630130+slaimmials@users.noreply.github.com>
2023-11-12 17:44:53 +0300

    v1

---
+++ menu.lua
@@ -0,0 +1,409 @@
+local wKjfpDklMZmTZkS = {["Frame_1"] = Instance.new("Frame"), ["TextLabel_1"] = Instance.new("TextLabel"), ["TextButton_1"] = Instance.new("TextButton"), ["LocalScript_1"] = Instance.new("LocalScript"), ["LocalScript_2"] = Instance.new("LocalScript"), ["Frame_2"] = Instance.new("Frame"), ["Frame_3"] = Instance.new("Frame"), ["TextButton_2"] = Instance.new("TextButton"), ["LocalScript_3"] = Instance.new("LocalScript"), ["TextButton_3"] = Instance.new("TextButton"), ["LocalScript_4"] = Instance.new("LocalScript"), ["TextButton_4"] = Instance.new("TextButton"), ["LocalScript_5"] = Instance.new("LocalScript"), ["TextButton_5"] = Instance.new("TextButton"), ["LocalScript_6"] = Instance.new("LocalScript"), ["ScreenGui_1"] = Instance.new("ScreenGui")}
+
+--
+
+wKjfpDklMZmTZkS.ScreenGui_1.Parent = game.StarterGui
+
+----------------------------------------------------------------------------------------------------
+
+wKjfpDklMZmTZkS.Frame_1.BackgroundColor3 = Color3.new(0.231373, 0.231373, 0.231373)
+wKjfpDklMZmTZkS.Frame_1.BorderColor3 = Color3.new(0, 0, 0)
+wKjfpDklMZmTZkS.Frame_1.BorderSizePixel = 0
+wKjfpDklMZmTZkS.Frame_1.Position = UDim2.new(0.129226744, 0, 0.271110147, 0)
+wKjfpDklMZmTZkS.Frame_1.Size = UDim2.new(0, 342, 0, 553)
+wKjfpDklMZmTZkS.Frame_1.Name = [[Main]]
+wKjfpDklMZmTZkS.Frame_1.Parent = wKjfpDklMZmTZkS.ScreenGui_1
+
+wKjfpDklMZmTZkS.TextLabel_1.FontFace = Font.new("rbxasset://fonts/families/SourceSansPro.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+wKjfpDklMZmTZkS.TextLabel_1.Text = "Trident [0.1A]"
+wKjfpDklMZmTZkS.TextLabel_1.TextColor3 = Color3.new(1, 1, 1)
+wKjfpDklMZmTZkS.TextLabel_1.TextSize = 16
+wKjfpDklMZmTZkS.TextLabel_1.BackgroundColor3 = Color3.new(0.203922, 0.203922, 0.203922)
+wKjfpDklMZmTZkS.TextLabel_1.BorderColor3 = Color3.new(0, 0, 0)
+wKjfpDklMZmTZkS.TextLabel_1.BorderSizePixel = 0
+wKjfpDklMZmTZkS.TextLabel_1.Size = UDim2.new(0, 342, 0, 19)
+wKjfpDklMZmTZkS.TextLabel_1.Name = [[Title]]
+wKjfpDklMZmTZkS.TextLabel_1.Parent = wKjfpDklMZmTZkS.Frame_1
+
+wKjfpDklMZmTZkS.TextButton_1.FontFace = Font.new("rbxasset://fonts/families/SourceSansPro.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+wKjfpDklMZmTZkS.TextButton_1.Text = [[x]]
+wKjfpDklMZmTZkS.TextButton_1.TextColor3 = Color3.new(1, 1, 1)
+wKjfpDklMZmTZkS.TextButton_1.TextSize = 17
+wKjfpDklMZmTZkS.TextButton_1.BackgroundColor3 = Color3.new(0.690196, 0.164706, 0.164706)
+wKjfpDklMZmTZkS.TextButton_1.BorderColor3 = Color3.new(0, 0, 0)
+wKjfpDklMZmTZkS.TextButton_1.BorderSizePixel = 0
+wKjfpDklMZmTZkS.TextButton_1.Position = UDim2.new(0.943121672, 0, 0, 0)
+wKjfpDklMZmTZkS.TextButton_1.Size = UDim2.new(0, 19, 0, 19)
+wKjfpDklMZmTZkS.TextButton_1.Name = [[Close]]
+wKjfpDklMZmTZkS.TextButton_1.Parent = wKjfpDklMZmTZkS.Frame_1
+
+wKjfpDklMZmTZkS.LocalScript_1.Parent = wKjfpDklMZmTZkS.TextButton_1
+
+wKjfpDklMZmTZkS.LocalScript_2.Name = [[MoveScript]]
+wKjfpDklMZmTZkS.LocalScript_2.Parent = wKjfpDklMZmTZkS.Frame_1
+
+wKjfpDklMZmTZkS.Frame_2.BackgroundColor3 = Color3.new(1, 1, 1)
+wKjfpDklMZmTZkS.Frame_2.BackgroundTransparency = 1
+wKjfpDklMZmTZkS.Frame_2.BorderColor3 = Color3.new(0, 0, 0)
+wKjfpDklMZmTZkS.Frame_2.BorderSizePixel = 0
+wKjfpDklMZmTZkS.Frame_2.Position = UDim2.new(0, 0, 0.0687160939, 0)
+wKjfpDklMZmTZkS.Frame_2.Size = UDim2.new(1, 0, 0.930999994, 0)
+wKjfpDklMZmTZkS.Frame_2.Name = [[Functions]]
+wKjfpDklMZmTZkS.Frame_2.Parent = wKjfpDklMZmTZkS.Frame_1
+
+wKjfpDklMZmTZkS.Frame_3.BackgroundColor3 = Color3.new(1, 1, 1)
+wKjfpDklMZmTZkS.Frame_3.BackgroundTransparency = 1
+wKjfpDklMZmTZkS.Frame_3.BorderColor3 = Color3.new(0, 0, 0)
+wKjfpDklMZmTZkS.Frame_3.BorderSizePixel = 0
+wKjfpDklMZmTZkS.Frame_3.Size = UDim2.new(1, 0, 1, 0)
+wKjfpDklMZmTZkS.Frame_3.Visible = false
+wKjfpDklMZmTZkS.Frame_3.Name = [[Visuals]]
+wKjfpDklMZmTZkS.Frame_3.Parent = wKjfpDklMZmTZkS.Frame_2
+
+wKjfpDklMZmTZkS.TextButton_2.FontFace = Font.new("rbxasset://fonts/families/SourceSansPro.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+wKjfpDklMZmTZkS.TextButton_2.Text = [[         Players ESP]]
+wKjfpDklMZmTZkS.TextButton_2.TextColor3 = Color3.new(1, 1, 1)
+wKjfpDklMZmTZkS.TextButton_2.TextSize = 20
+wKjfpDklMZmTZkS.TextButton_2.TextXAlignment = Enum.TextXAlignment.Left
+wKjfpDklMZmTZkS.TextButton_2.BackgroundColor3 = Color3.new(0.529412, 0.133333, 0.133333)
+wKjfpDklMZmTZkS.TextButton_2.BorderColor3 = Color3.new(0.184314, 0.184314, 0.184314)
+wKjfpDklMZmTZkS.TextButton_2.BorderSizePixel = 3
+wKjfpDklMZmTZkS.TextButton_2.Position = UDim2.new(0.0584795326, 0, 0.0388467945, 0)
+wKjfpDklMZmTZkS.TextButton_2.Size = UDim2.new(0, 20, 0, 20)
+wKjfpDklMZmTZkS.TextButton_2.Name = [[PlayersESP]]
+wKjfpDklMZmTZkS.TextButton_2.Parent = wKjfpDklMZmTZkS.Frame_3
+
+wKjfpDklMZmTZkS.LocalScript_3.Parent = wKjfpDklMZmTZkS.TextButton_2
+
+wKjfpDklMZmTZkS.TextButton_3.FontFace = Font.new("rbxasset://fonts/families/SourceSansPro.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+wKjfpDklMZmTZkS.TextButton_3.Text = [[Visuals]]
+wKjfpDklMZmTZkS.TextButton_3.TextColor3 = Color3.new(1, 1, 1)
+wKjfpDklMZmTZkS.TextButton_3.TextSize = 16
+wKjfpDklMZmTZkS.TextButton_3.BackgroundColor3 = Color3.new(0.215686, 0.215686, 0.215686)
+wKjfpDklMZmTZkS.TextButton_3.BorderColor3 = Color3.new(0, 0, 0)
+wKjfpDklMZmTZkS.TextButton_3.BorderSizePixel = 0
+wKjfpDklMZmTZkS.TextButton_3.Position = UDim2.new(0, 0, 0.0340000018, 0)
+wKjfpDklMZmTZkS.TextButton_3.Size = UDim2.new(0.335000008, 0, 0, 19)
+wKjfpDklMZmTZkS.TextButton_3.Name = [[Visuals]]
+wKjfpDklMZmTZkS.TextButton_3.Parent = wKjfpDklMZmTZkS.Frame_1
+
+wKjfpDklMZmTZkS.LocalScript_4.Parent = wKjfpDklMZmTZkS.TextButton_3
+
+wKjfpDklMZmTZkS.TextButton_4.FontFace = Font.new("rbxasset://fonts/families/SourceSansPro.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+wKjfpDklMZmTZkS.TextButton_4.Text = [[Player]]
+wKjfpDklMZmTZkS.TextButton_4.TextColor3 = Color3.new(1, 1, 1)
+wKjfpDklMZmTZkS.TextButton_4.TextSize = 16
+wKjfpDklMZmTZkS.TextButton_4.BackgroundColor3 = Color3.new(0.215686, 0.215686, 0.215686)
+wKjfpDklMZmTZkS.TextButton_4.BorderColor3 = Color3.new(0, 0, 0)
+wKjfpDklMZmTZkS.TextButton_4.BorderSizePixel = 0
+wKjfpDklMZmTZkS.TextButton_4.Position = UDim2.new(0.333000004, 0, 0.0340000018, 0)
+wKjfpDklMZmTZkS.TextButton_4.Size = UDim2.new(0.335000008, 0, 0, 19)
+wKjfpDklMZmTZkS.TextButton_4.Name = [[Player]]
+wKjfpDklMZmTZkS.TextButton_4.Parent = wKjfpDklMZmTZkS.Frame_1
+
+wKjfpDklMZmTZkS.LocalScript_5.Parent = wKjfpDklMZmTZkS.TextButton_4
+
+wKjfpDklMZmTZkS.TextButton_5.FontFace = Font.new("rbxasset://fonts/families/SourceSansPro.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+wKjfpDklMZmTZkS.TextButton_5.Text = [[Combat]]
+wKjfpDklMZmTZkS.TextButton_5.TextColor3 = Color3.new(1, 1, 1)
+wKjfpDklMZmTZkS.TextButton_5.TextSize = 16
+wKjfpDklMZmTZkS.TextButton_5.BackgroundColor3 = Color3.new(0.215686, 0.215686, 0.215686)
+wKjfpDklMZmTZkS.TextButton_5.BorderColor3 = Color3.new(0, 0, 0)
+wKjfpDklMZmTZkS.TextButton_5.BorderSizePixel = 0
+wKjfpDklMZmTZkS.TextButton_5.Position = UDim2.new(0.661000013, 0, 0.0340000018, 0)
+wKjfpDklMZmTZkS.TextButton_5.Size = UDim2.new(0.335000008, 0, 0, 19)
+wKjfpDklMZmTZkS.TextButton_5.Name = [[Combat]]
+wKjfpDklMZmTZkS.TextButton_5.Parent = wKjfpDklMZmTZkS.Frame_1
+
+wKjfpDklMZmTZkS.LocalScript_6.Parent = wKjfpDklMZmTZkS.TextButton_5
+
+wKjfpDklMZmTZkS.ScreenGui_1.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+wKjfpDklMZmTZkS.ScreenGui_1.Name = [[Trident]]
+----------------------------------------------------------------------------------------------------
+
+local function LocalScript_1(script)
+
+	script.Parent.TouchTap:Connect(function()
+		script.Parent.Parent:Destroy()
+	end)
+end
+
+----------------------------------------------------------------------------------------------------
+
+local function LocalScript_4(script)
+
+	local State = false
+	script.Parent.TouchTap:Connect(function()
+		State = not State
+		if State then
+			for _,v in pairs(script.Parent.Parent.Functions:GetChildren()) do
+				if v.Name ~= script.Parent.Name then
+					v.Visible = false
+				else
+					v.Visible = true
+				end
+			end
+			script.Parent.Parent.Player.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+			script.Parent.Parent.Combat.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+			script.Parent.BackgroundColor3 = Color3.fromRGB(59, 59, 59)
+		else
+			script.Parent.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+		end
+	end)
+end
+
+----------------------------------------------------------------------------------------------------
+
+local function LocalScript_5(script)
+
+	local State = false
+	script.Parent.TouchTap:Connect(function()
+		State = not State
+		if State then
+			for _,v in pairs(script.Parent.Parent.Functions:GetChildren()) do
+				if v.Name ~= script.Parent.Name then
+					v.Visible = false
+				else
+					v.Visible = true
+				end
+			end
+			script.Parent.Parent.Visuals.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+			script.Parent.Parent.Combat.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+			script.Parent.BackgroundColor3 = Color3.fromRGB(59, 59, 59)
+		else
+			script.Parent.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+		end
+	end)
+end
+
+----------------------------------------------------------------------------------------------------
+
+local function LocalScript_3(script)
+
+	local State = false
+	local essp = Instance.new("BillboardGui")
+	local Up = Instance.new("Frame")
+	local Left = Instance.new("Frame")
+	local Right = Instance.new("Frame")
+	local Down = Instance.new("Frame")
+	local WHR = false
+	
+	essp.Parent = script
+	essp.Name = "Esp"
+	essp.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+	essp.Active = true
+	essp.Adornee = game.Workspace.Rigs.Riga
+	essp.AlwaysOnTop = true
+	essp.LightInfluence = 1.000
+	essp.Size = UDim2.new(2.5, 0, 5, 0)
+	
+	Up.Name = "Up"
+	Up.Parent = essp
+	Up.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	Up.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	Up.BorderSizePixel = 0
+	Up.Size = UDim2.new(1.00980389, 0, 0.024271844, 0)
+	
+	Left.Name = "Left"
+	Left.Parent = essp
+	Left.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	Left.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	Left.BorderSizePixel = 0
+	Left.Size = UDim2.new(0.0490196086, 0, 1, 0)
+	
+	Right.Name = "Right"
+	Right.Parent = essp
+	Right.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	Right.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	Right.BorderSizePixel = 0
+	Right.Position = UDim2.new(0.960784316, 0, 0, 0)
+	Right.Size = UDim2.new(0.0490196086, 0, 1, 0)
+	
+	Down.Name = "Down"
+	Down.Parent = essp
+	Down.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	Down.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	Down.BorderSizePixel = 0
+	Down.Position = UDim2.new(0, 0, 0.975300014, 0)
+	Down.Size = UDim2.new(1.00980389, 0, 0.024271844, 0)
+	
+	script.Parent.TouchTap:Connect(function()
+		State = not State
+		if State then
+			script.Parent.BackgroundColor3 = Color3.fromRGB(38, 135, 33)
+		else
+			script.Parent.BackgroundColor3 = Color3.fromRGB(135, 34, 34)
+		end
+	end)
+	
+	while wait(0.1) do
+		pcall(function()
+			if State then
+				for _,v in pairs(game.Players:GetPlayers()) do
+					if v.Name ~= game.Players.LocalPlayer.Name then
+						if not v.Character:FindFirstChild("Esp") then
+							local esp = essp:Clone()
+							esp.Parent = v.Character
+							esp.Adornee = v.Character
+						end
+					end
+				end
+				WHR = false
+			elseif WHR == false then
+				WHR = true
+				for _,v in pairs(game.Players:GetPlayers()) do
+					if v.Character:FindFirstChild("Esp") then 
+						v.Character:FindFirstChild("Esp"):Remove()
+					end
+				end
+			end
+		end)
+	end
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+end
+
+----------------------------------------------------------------------------------------------------
+
+local function LocalScript_2(script)
+
+	local UIS = game:GetService('UserInputService')
+	local frame = script.Parent
+	local dragToggle = nil
+	local Dragging = nil
+	local dragSpeed = 0.1
+	local dragStart = nil
+	local startPos = nil
+	
+	UIS.InputBegan:Connect(function(input, typing)
+		if typing then
+			return
+		end
+	
+		if input.KeyCode == Enum.KeyCode.F12 then
+			script.Parent.Visible = not script.Parent.Visible
+		end
+	end)
+	
+	local function updateInput(input)
+		local delta = input.Position - dragStart
+		local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
+			startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+		game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
+	end
+	
+	frame.InputBegan:Connect(function(input)
+		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
+			dragToggle = true
+			Dragging = true
+			dragStart = input.Position
+			startPos = frame.Position
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragToggle = false
+					Dragging = false
+				end
+			end)
+		end
+	end)
+	
+	UIS.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			if dragToggle then
+				updateInput(input)
+			end
+		end
+	end)
+end
+
+----------------------------------------------------------------------------------------------------
+
+local function LocalScript_6(script)
+
+	local State = false
+	script.Parent.TouchTap:Connect(function()
+		State = not State
+		if State then
+			for _,v in pairs(script.Parent.Parent.Functions:GetChildren()) do
+				if v.Name ~= script.Parent.Name then
+					v.Visible = false
+				else
+					v.Visible = true
+				end
+			end
+			script.Parent.Parent.Player.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+			script.Parent.Parent.Visuals.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+			script.Parent.BackgroundColor3 = Color3.fromRGB(59, 59, 59)
+		else
+			script.Parent.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+		end
+	end)
+end
+
+----------------------------------------------------------------------------------------------------
+
+coroutine.wrap(LocalScript_1)(wKjfpDklMZmTZkS.LocalScript_1)
+coroutine.wrap(LocalScript_4)(wKjfpDklMZmTZkS.LocalScript_4)
+coroutine.wrap(LocalScript_5)(wKjfpDklMZmTZkS.LocalScript_5)
+coroutine.wrap(LocalScript_3)(wKjfpDklMZmTZkS.LocalScript_3)
+coroutine.wrap(LocalScript_2)(wKjfpDklMZmTZkS.LocalScript_2)
+coroutine.wrap(LocalScript_6)(wKjfpDklMZmTZkS.LocalScript_6)
